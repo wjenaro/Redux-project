421e0ffd371e090302771c56092bfee737a9ab0d
wjenaro <wjenaro@gmail.com>
2024-02-06 22:50:32 +0300

    store

---
+++ src/state/store.ts
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { type } from "os";
+export const store=configureStore({
+    reducer: {},
+});
+export type RootState=ReturnType<typeof store.getState>;
+export type AppDispatch =typeof store.dispatch;
